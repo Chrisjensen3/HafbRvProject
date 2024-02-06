386a68dea141a67b21599aa825666dd54ecbde0e
Chrisjensen3 <108089473+Chrisjensen3@users.noreply.github.com>
2024-02-06 10:24:03 -0700

    Update connectionInfo.js

--- RvProjectSite/rvproject/connectionInfo.js
+++ RvProjectSite/rvproject/connectionInfo.js
@@ -1,7 +1,7 @@
 var connectionInfo = {
     host: 'MYSQL5048.site4now.net',
     port: 3306, // Default MySQL port. You might need to change this if your server uses a different port.
-    user: 'a9e9f2_rvpark',
-    password: 'temp1234!'
+    user: 'a9e9f2_admin',
+    password: 'Admin1234'
 };
 module.exports = connectionInfo;
