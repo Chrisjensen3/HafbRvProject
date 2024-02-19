7e53a38a2a682f45ae7cef0f69e542f5fd710dca
Chris Jensen <chrisjensen3@mail.weber.edu>
2024-02-19 08:58:09 -0800

    rm coninfo

---
+++ RvProjectSite/rvproject/connectionInfo.js
@@ -0,0 +1,8 @@
+var connectionInfo = {
+    host: 'mysql5026.site4now.net',
+    port: 3306,
+    user: 'a9e9f2_parkrv',
+    password: 'temp1234',
+    database: 'db_a9e9f2_parkrv' 
+};
+module.exports = connectionInfo;
